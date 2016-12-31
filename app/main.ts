77e371535de6e6fac0e63829e4871137d6fe8404
wesniem <wesniem@yahoo.com>
2016-12-31 15:06:13 -0500

    New commit

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+//This is a browser function
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);

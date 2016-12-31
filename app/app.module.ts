b605c888ceac89146f367d2500cdcf14e6a9c32c
wesniem <wesniem@yahoo.com>
2016-12-31 18:15:26 -0500

    ngIf/For and Pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import {FavoriteDirective} from './favorite.directive';
+import {CategoryListPipe} from './category-list.pipe';
 
 @NgModule({
   imports: [
@@ -12,7 +14,9 @@ import { MediaItemListComponent } from './media-item-list.component';
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe
   ],
   bootstrap: [
     AppComponent

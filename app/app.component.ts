5a2acc6189d3a411ffc0df86b72969865621ea59
wesniem <wesniem@yahoo.com>
2016-12-31 15:41:34 -0500

    Using Browser Module and style/template URLs

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl:'app/app.component.html'
+  templateUrl:'app/app.component.html',
+    //Sets up style content The URL allows you to grab the css code from the css file
+    styleUrls:['app/app.component.css']
 })
 export class AppComponent {}

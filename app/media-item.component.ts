448d903b0a7490bacc008d46d9ae68d1a459772f
wesniem <wesniem@yahoo.com>
2016-12-31 16:43:35 -0500

    Property/ Event Binding

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  //allows you to use input from above
+  @Input() mediaItem;
+
+  onDelete(){
+    console.log("Deleted");
+  }
+//   name = 'The Redemption';
+// // This method can be called in the HTML file instead of hard coding it
+//   wasWatched(){
+//     return true;
+//   }
+}

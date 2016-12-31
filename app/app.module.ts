28560d5340f35512eaa973c3ab4527d60f726044
wesniem <wesniem@yahoo.com>
2016-12-31 18:29:46 -0500

    Template Driven Form

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,27 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
+import {AppComponent} from './app.component';
+import {MediaItemComponent} from './media-item.component';
+import {MediaItemListComponent} from './media-item-list.component';
+import {FavoriteDirective} from './favorite.directive';
+import {CategoryListPipe} from './category-list.pipe';
+import {MediaItemFormComponent} from './media-item-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent,
-    MediaItemComponent,
-    MediaItemListComponent,
-    FavoriteDirective,
-    CategoryListPipe,
-    MediaItemFormComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavoriteDirective,
+        CategoryListPipe,
+        MediaItemFormComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}

5af86eeebe22bce13f1bdbafc7a046e6366f5f16
wesniem <wesniem@yahoo.com>
2016-12-31 18:44:39 -0500

    Model Driven form with builtin validation

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -6,6 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+  form;
+
+
+  ngOnInit(){
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: new FormControl(''),
+      year: new FormControl(''),
+    });
+  }
+
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }

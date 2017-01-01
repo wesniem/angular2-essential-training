fb34a4834b8f35f17aaa6677b484c3d249b5fe57
wesniem <wesniem@yahoo.com>
2017-01-01 15:20:43 -0500

    Inject decorator

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,52 +1,53 @@
-import { Component } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
+import {Component, Inject} from '@angular/core';
+import {Validators, FormBuilder} from '@angular/forms';
 
-import { MediaItemService } from './media-item.service';
+import {MediaItemService} from './media-item.service';
 
 @Component({
-  selector: 'mw-media-item-form',
-  templateUrl: 'app/media-item-form.component.html',
-  styleUrls: ['app/media-item-form.component.css']
+    selector: 'mw-media-item-form',
+    templateUrl: 'app/media-item-form.component.html',
+    styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
-  form;
+    form;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private mediaItemService: MediaItemService) {}
-
-  ngOnInit() {
-    this.form = this.formBuilder.group({
-      medium: this.formBuilder.control('Movies'),
-      name: this.formBuilder.control('', Validators.compose([
-        Validators.required,
-        Validators.pattern('[\\w\\-\\s\\/]+')
-      ])),
-      category: this.formBuilder.control(''),
-      year: this.formBuilder.control('', this.yearValidator),
-    });
-  }
-
-  yearValidator(control) {
-    if (control.value.trim().length === 0) {
-      return null;
+    constructor(private formBuilder: FormBuilder,
+                private mediaItemService: MediaItemService,
+                @Inject('lookupListToken') public lookupLists) {
     }
-    let year = parseInt(control.value);
-    let minYear = 1800;
-    let maxYear = 2500;
-    if (year >= minYear && year <= maxYear) {
-      return null;
-    } else {
-      return {
-        'year': {
-          min: minYear,
-          max: maxYear
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            medium: this.formBuilder.control('Movies'),
+            name: this.formBuilder.control('', Validators.compose([
+                Validators.required,
+                Validators.pattern('[\\w\\-\\s\\/]+')
+            ])),
+            category: this.formBuilder.control(''),
+            year: this.formBuilder.control('', this.yearValidator),
+        });
+    }
+
+    yearValidator(control) {
+        if (control.value.trim().length === 0) {
+            return null;
         }
-      };
+        let year = parseInt(control.value);
+        let minYear = 1800;
+        let maxYear = 2500;
+        if (year >= minYear && year <= maxYear) {
+            return null;
+        } else {
+            return {
+                'year': {
+                    min: minYear,
+                    max: maxYear
+                }
+            };
+        }
     }
-  }
 
-  onSubmit(mediaItem) {
-    this.mediaItemService.add(mediaItem);
-  }
+    onSubmit(mediaItem) {
+        this.mediaItemService.add(mediaItem);
+    }
 }
